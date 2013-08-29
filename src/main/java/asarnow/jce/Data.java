98de68e0c445913d975759dc5bb64fadf4fbb649
Daniel Asarnow <dasarnow@gmail.com>
2013-08-29 10:29:23 -0700

    Distance matrix function partially implemented

--- src/main/java/asarnow/jce/Data.java
+++ src/main/java/asarnow/jce/Data.java
@@ -5,12 +5,17 @@ import org.biojava.bio.structure.StructureException;
 import org.biojava.bio.structure.align.util.AtomCache;
 import org.biojava.bio.structure.io.FileParsingParameters;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Daniel Asarnow
@@ -67,6 +72,30 @@ public class Data {
     }
 
     public static String getStructureInfo(String id) {
+        return null;
+    }
+
+    public static double[] distanceMatrix(List<String> ids, String alignFilePath, int field) {
+        Map<String,Integer> index = new HashMap<>( ids.size() );
+        for (int i = 0; i < ids.size(); i++) {
+            index.put(ids.get(i),i);
+        }
+        File alignFile = new File(alignFilePath);
+        try (BufferedReader bis = new BufferedReader(new FileReader(alignFile))){
+            String[] tok = bis.readLine().split("\t");
+            String id1 = tok[0];
+            String id2 = tok[1];
+            Double val = Double.valueOf(tok[field]);
+
+
+
+
+        } catch (IOException e) {
+            //TODO auto-generated catch block
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 }
