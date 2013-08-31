d0f4c599d761a6a875e1d171ee8be167bfb1e9af
Daniel Asarnow <dasarnow@gmail.com>
2013-08-31 02:11:59 -0700

    Fixes in matrix, divided into more functions structureInfo implemented

--- src/main/java/asarnow/jce/Data.java
+++ src/main/java/asarnow/jce/Data.java
@@ -13,6 +13,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,7 +37,6 @@ public class Data {
                 Utility.writePDB(structure, extractDir, compressed);
                 newlist.add(structure.getName());
             } catch (IOException | StructureException e) {
-                //TODO auto-generated catch block
                 e.printStackTrace();
             }
         }
@@ -55,7 +55,6 @@ public class Data {
             Utility.writePDB(structure, extractDir, compressed);
             return structure.getName();
         } catch (IOException | StructureException e) {
-            //TODO auto-generated catch block
             e.printStackTrace();
         }
         return null;
@@ -80,22 +79,64 @@ public class Data {
         for (int i = 0; i < ids.size(); i++) {
             index.put(ids.get(i),i);
         }
+        int n = ids.size();
+        double[] matrix = new double[choose2(n)];
+        Arrays.fill(matrix, Double.NaN);
         File alignFile = new File(alignFilePath);
         try (BufferedReader bis = new BufferedReader(new FileReader(alignFile))){
-            String[] tok = bis.readLine().split("\t");
-            String id1 = tok[0];
-            String id2 = tok[1];
-            Double val = Double.valueOf(tok[field]);
-
-
-
-
+            String line;
+            while ((line = bis.readLine())!=null) {
+                String[] tok = line.split("\t");
+                String id1 = tok[0];
+                String id2 = tok[1];
+                if (!id1.equals(id2)) {
+                    Double val = Double.valueOf(tok[field]);
+                    int c = index.get(id1) < index.get(id2) ? index.get(id1) : index.get(id2);
+                    int r = index.get(id1) < index.get(id2) ? index.get(id2) : index.get(id1);
+                    matrix[ sub2idxflat(c,r,n) ] = val;
+                }
+            }
         } catch (IOException e) {
-            //TODO auto-generated catch block
             e.printStackTrace();
         }
+        return matrix;
+    }
 
+    public static int choose2(int n) {
+        return n*(n-1)/2;
+    }
 
-        return null;
+    public static int sub2idxflat(int c, int r, int n) {
+        int nch2 = choose2(n);
+        int ncch2 = choose2(n-c);
+        return nch2 - ncch2 + (r-c-1);
+    }
+
+    public static String[] ints2strings(int[] ints) {
+        String[] strings = new String[ints.length];
+        for (int i=0; i<ints.length; i++) strings[i] = Integer.toString(ints[i]);
+        return strings;
+    }
+
+    public static String[] doubles2strings(double[] ints) {
+        String[] strings = new String[ints.length];
+        for (int i=0; i<ints.length; i++) strings[i] = Double.toString(ints[i]);
+        return strings;
+    }
+
+    public static void printStructureInfo(List<String> ids, String pdbDir, Boolean divided, FileParsingParameters parameters) {
+        AtomCache cache = Utility.initAtomCache(pdbDir,divided);
+        for (String id : ids) {
+            try {
+                Structure structure = cache.getStructure(id);
+                String info = "Structure specification " + id + " yields\n" +
+                        structure.getPDBHeader().toPDB() + "\n";
+                System.out.print(info);
+            } catch (IOException | StructureException e) {
+                System.out.println("Exception on structure specification " + id);
+                e.printStackTrace();
+            }
+        }
+
     }
 }
